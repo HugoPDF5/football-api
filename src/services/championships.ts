0c3f74943852c181a864642a3255867588bd76da
Hugo <hugopatricio51@gmail.com>
2023-02-06 18:01:26 -0300

    feat: adding new service layer

--- src/services/championships.ts
+++ src/services/championships.ts
@@ -1,25 +1,76 @@
-import { api } from './api'
+import { championshipIdProps, championshipProps, roundProps, strikersProps } from "../types/championship";
+import { api } from "./api";
 
 export function useChampionships() {
-    const getAllChampionships = async () => {
-        const response = await api.get('campeonatos')
-        return response
-    }
+  const baseUrl = 'campeonatos'
+  const getAllChampionships = async (): Promise<championshipProps[] | []> => {
+    const { data } = await api.get(baseUrl);
+    return data.map((item: championshipProps) => {
+      return {
+        campeonato_id: item.campeonato_id,
+        nome: item.nome,
+        slug: item.slug,
+        nome_popular: item.nome_popular,
+        edicao_atual: item.edicao_atual,
+        fase_atual: item.fase_atual,
+        rodada_atual: item.rodada_atual,
+        status: item.status,
+        logo: item.logo,
+        _link: item._link,
+      };
+    });
+  };
 
-    const getChampionshipById = async (id: string) => {
-        const response = await api.get(`campeonatos/${id}`)
-        return response
-    }
+  const getChampionshipById = async (id: string): Promise<championshipIdProps> => {
+    const { data } = await api.get(`${baseUrl}/${id}`);
+    return data.map((champ: championshipIdProps) => {
+      return {
+        tipo: champ.tipo,
+        regiao: champ.regiao,
+        fases: champ.fases,
+        campeonato_id: champ.campeonato_id,
+        nome: champ.nome,
+        slug: champ.slug,
+        nome_popular: champ.nome_popular,
+        edicao_atual: champ.edicao_atual,
+        fase_atual: champ.fase_atual,
+        rodada_atual: champ.rodada_atual,
+        status: champ.status,
+        logo: champ.logo,
+        _link: champ._link,
+      };
+    });
+  };
 
-    const getTableByChampionshipId = async (id: string) => {
-        const response = await api.get(`campeonatos/${id}/tabela`)
-        return response
-    }
+  const getTableByChampionshipId = async (id: string) => {
+    const response = await api.get(`${baseUrl}/${id}/tabela`);
+    return response;
+  };
 
+  const getStrikersByChampionshipId = async (id: string): Promise<strikersProps> => {
+    const { data } = await api.get(`${baseUrl}/${id}/artilharia`);
+    return data.map((player: strikersProps) => {
+      return {
+        atleta: player.atleta,
+        time: player.time,
+        gols: player.gols,
+      };
+    });
+  };
 
-    return {
-        getAllChampionships,
-        getChampionshipById,
-        getTableByChampionshipId
-    }
-} 
+  const getRoundById = async (champId: number, roundId: number): Promise<roundProps> => {
+    const { data } = await api.get(`${baseUrl}/${champId}/rodadas/${roundId}`)
+      return {
+        partidas: data.partidas,
+        nome: data.nome
+      }
+  }
+
+  return {
+    getAllChampionships,
+    getChampionshipById,
+    getTableByChampionshipId,
+    getStrikersByChampionshipId,
+    getRoundById
+  };
+}
